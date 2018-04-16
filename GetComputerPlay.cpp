474f84a157bfcff43c215ed72b19063603e5c022
farahalfarah <38386797+farahalfarah@users.noreply.github.com>
2018-04-16 00:30:10 -0700

    Create GetComputerPlay.cpp

---
+++ GetComputerPlay.cpp
@@ -0,0 +1,14 @@
+/*******************************************************************************
+ * AUTHORS		: Zac Campos & Farah Alfarah
+ * STUDENT ID	: 1058476   & 1035501
+ * LAB #23 		: Functions - Rock Paper Scissors
+ * CLASS		: CS1A
+ * SECTION		: MW 8am
+ * DUE DATE		: 4/16/18
+ ******************************************************************************/
+
+/**************************************************************************
+ * Function GetComputerPlay: This function is seperated on another source 
+ * file. It randomly selects a play for the computer and returns the 
+ * character representing their play.
+ *************************************************************************/
